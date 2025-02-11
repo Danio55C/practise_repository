2057c1d7e91e7ab67d3ea906cf75e21596900fbb
daniel Janca <jancadaniel55@gmail.com>
2025-02-11 20:20:30 +0100

    added script for python and redo docker compose (doesn't work yet)

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 connection = mysql.connector.connect(
     user="root", password="root", host="mysql", port="3306", database="userdb")
 print("DB connected")
@@ -11,3 +12,20 @@ connection.close()
 
 
 print(users)
+###########################################################
+from kafka import KafkaProducer, KafkaConsumer
+
+
+producer = KafkaProducer(bootstrap_servers="broker:19092")
+producer.send("test-topic", b"Hello from Python!")
+producer.flush()
+
+consumer = KafkaConsumer("test-topic", bootstrap_servers="broker:19092")
+for message in consumer:
+    print(f"Received message: {message.value}")
+
+
+producer.close()
+consumer.close()
+
+
