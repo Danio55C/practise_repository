8a7cff8ef6412b2f1a8ce05bc066969e34452645
daniel Janca <jancadaniel55@gmail.com>
2025-02-23 21:09:14 +0100

    start of data exploration

--- python/main.py
+++ python/main.py
@@ -10,17 +10,32 @@ import hashlib
 import uuid
 from loguru import logger
 import os
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-alert_names = ["Memory Leak", "Network Issue", "Too Many Connections", "Database Connection Lost", "Missing Index Warning", "Inconsistent Data Found"]
+start_time = time.time()
 
-severity_levels = [ "Warning", "Error", "Critical Error"]
+#mysql Database configuration
+my_config = {
+    "user_name": "root",
+    "password": "root",
+    "host": "mysql",
+    "port": "3306",
+    "database_name": "userdb"
+}
 
+#log file and config
 log_file_path = "/usr/app/src/logs/file_logs.log"
 os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
 logger.add(log_file_path, rotation="1 week", level="WARNING", format='{{"time": "{time}", "level": "{level}", "message": "{message}"}}')
 logger.info("Logger initialized, writing logs to file_logs.log")
+
+#generate alerts 
+alert_names = ["Memory Leak", "Network Issue", "Too Many Connections", "Database Connection Lost", "Missing Index Warning", "Inconsistent Data Found"]
+severity_levels = [ "Warning", "Error", "Critical Error"]
 
 def generate_alert():
     alert_name = random.choice(alert_names)
@@ -35,6 +50,7 @@ def generate_alert():
         "message": message
     }
 
+#mysql connection
 def create_mysqlconnection(user_name, password, host, port, database_name):
     connection = None
     try:
@@ -50,24 +66,7 @@ def create_mysqlconnection(user_name, password, host, port, database_name):
         logger.error(f"MySQL - Error occurred while trying to connect to DB: '{e}'")
     return connection 
 
-#mysql Database configuration
-config = {
-    "user_name": "root",
-    "password": "root",
-    "host": "mysql",
-    "port": "3306",
-    "database_name": "userdb"
-}
-
-db_connection = create_mysqlconnection(**config)
-cursor = db_connection.cursor()
-
-# insert alerts sql querry
-insert_alert_query = ("INSERT INTO alerts "
-                      "(AlertName, SeverityLevel, Timestamp, Message) "
-                      "VALUES (%(alert_name)s, %(severity_level)s, %(timestamp)s, %(message)s)")
-
-# **Kafka and elsticsearch config**
+#kafka producer config and connection
 try:
     Topic_Name = 'alerts'
     producer = KafkaProducer(
@@ -78,27 +77,36 @@ try:
 except Exception as e:
     logger.error(f"Kafka - Error has ocured while connection to broker: {e}")
 
-
-
+#elasticsearch config and connection
 try:
-    client = Elasticsearch("http://elasticsearch:9200")
+    client_es = Elasticsearch("http://elasticsearch:9200")
     logger.info("Elsticsearch - Connection to DB successful")
 except Error as e:
     logger.error(f"ElasticSearch - Error has ocured while trying to connect to DB: '{e}'")
+
+
+# insert alerts sql querry
+insert_alert_query = ("INSERT INTO alerts "
+                      "(AlertName, SeverityLevel, Timestamp, Message) "
+                      "VALUES (%(alert_name)s, %(severity_level)s, %(timestamp)s, %(message)s)")
+
+
+db_connection = create_mysqlconnection(**my_config)
+cursor = db_connection.cursor()
 
 # **Generate random alerts and send them to mysql kafka and elasticsearch**
 for x in range(6):
     data_alerts = generate_alert()
     producer.send(Topic_Name, value=data_alerts["message"])
     cursor.execute(insert_alert_query, data_alerts)
-    client.index(index="alerts", id=x, document=data_alerts)
+    client_es.index(index="alerts", id=str(uuid.uuid4()), document=data_alerts)
 
 producer.flush()
 db_connection.commit()
 logger.info("Data inserted successfully\n")
 
 
-# **Kafka Consumer messages**
+# **Consuming messages kafka**
 consumer = KafkaConsumer(
     Topic_Name,
     group_id="alerts",
@@ -107,21 +115,21 @@ consumer = KafkaConsumer(
     auto_offset_reset='earliest', enable_auto_commit=False,
     consumer_timeout_ms=5000
 )
-
 # for message_alerts in consumer:
 #     logger.error(f"Consumer received message: {message_alerts.value}\n")
+
     
-
-
 # **Retrieve related data form MYSQL or Memcached**
-client_memcached = Client(("memcached", 11211))
+retriving_related_data_process_start = time.time()
+
+client_memcached = Client(("memcached", 11211))  ##memcached config
 alert_id = 3
 cache_key = f"alerts_{alert_id}"
 alerts_memcached = client_memcached.get(cache_key) 
 sql_alerts_query = "SELECT * FROM alerts WHERE Alertid = %s"
 
 if not alerts_memcached:
-    logger.info("\nData not found in cache, searching in MySQL database: ") 
+    logger.info("\nData not found in cache, searching in MySQL database...") 
     cursor.execute(sql_alerts_query, (alert_id,)) 
     alerts_memcached = cursor.fetchall()
     client_memcached.set(cache_key, alerts_memcached, expire=300)
@@ -129,6 +137,8 @@ if not alerts_memcached:
 else: 
     logger.debug(f"\nData found in cache:\n{alerts_memcached}")
 
+retriving_related_data_process_end = time.time()
+logger.info(f"Retriving data took: {retriving_related_data_process_end - retriving_related_data_process_start:.4f} seconds.")
 
 # **Enrichinh data - Adding risk score and hashfield**
 cursor.execute("SHOW COLUMNS FROM alerts LIKE 'HashField'")
@@ -144,15 +154,17 @@ if not exists:
 db_connection.commit()
 
 cursor.execute("SELECT Alertid, AlertName, SeverityLevel, Timestamp, Message FROM alerts")
-alerts = cursor.fetchall()
+alerts_mysql = cursor.fetchall()
 
 severity_mapping = {
     "Warning": 1,
     "Error": 4,
     "Critical": 8
 }
+logger.info(f"\nStarting alerts enrichment process: ")
+alerts_processing_start = time.time()
 
-for alert in alerts:
+for alert in alerts_mysql:
     alert_id, alert_name, severity_level, timestamp, message = alert
     cursor.execute("SELECT COUNT(*) FROM alerts WHERE AlertName = %s", (alert_name,))
     past_alerts_count = cursor.fetchone()[0]
@@ -175,6 +187,8 @@ for alert in alerts:
 
 db_connection.commit()
 
+alerts_processing_end = time.time()
+logger.info(f"Processing alerts took {alerts_processing_end - alerts_processing_start:.4f} seconds.")
 
 # **Sending enriched data back to kafka and elasticsearch**
 cursor.execute("SELECT * FROM alerts")
@@ -185,9 +199,10 @@ enriched_data = [dict(zip(columns, row)) for row in rows]
 print(" ")
 logger.debug(enriched_data)
 
+
 for alert in enriched_data:
     producer.send(Topic_Name, value=alert["Message"])
-    client.index(index="alerts", id=str(uuid.uuid4()), document=alert)
+    client_es.index(index="alerts", id=str(uuid.uuid4()), document=alert)
     if alert["SeverityLevel"] == "Warning":
         logger.warning(f"{alert['Message']}\n")
     if alert["SeverityLevel"] == "Error":
@@ -196,17 +211,40 @@ for alert in enriched_data:
         logger.critical(f"{alert['Message']}\n")      
 
 producer.flush()
-logger.info("\nData enriched")
+logger.info("\nData enriched\n")
+
+
+# **Exploring alert data**
+enriched_data_df = pd.DataFrame.from_dict(enriched_data)
+print(enriched_data_df)
+
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.axis('tight')
+ax.axis('off')
+
+table = plt.table(cellText=enriched_data_df.values, 
+                  colLabels=enriched_data_df.columns, 
+                  cellLoc='center', 
+                  loc='center')
+
+vizualization_file_path = "/usr/app/src/output/dataframe_output.png"
+os.makedirs(os.path.dirname(vizualization_file_path), exist_ok=True)
+plt.savefig(vizualization_file_path, dpi=300)
+
+
 
 
 # **closing connections**
 producer.close()
 consumer.close()
-client.close()
+client_es.close()
 
 cursor.close()
 db_connection.close()
-logger.info("\nEnd of the script")
+
+end_time = time.time()
+logger.info("End of the script")
+logger.info(f"\nTotal execution time: {end_time - start_time:.4f} seconds.")
 
 
 
